3c9c31569d7726303d161b5e65b1ed68a039c106
abc <a@b.c>
2022-12-11 22:34:26 +0800

    add 1211

--- testgui.py
+++ testgui.py
@@ -1,44 +1,48 @@
-import pyautogui
-import time
-import random
+# import pyautogui
+# import time
+# import random
 
 
-def xx():
-    try:
-        pyautogui.PAUSE = 1
-        pyautogui.FAILSAFE = True
-        width, height = pyautogui.size()
-        while True:
-            # x, y = pyautogui.position()
-            # s = "X: " + str(x).rjust(4) + " Y: " + str(y) .rjust(4)
-            # print(s, end="")
-            # print("\b" * len(s), end="", flush=True)
-            x = random.randint(1, width-1)
-            y = random.randint(1, height-1)
-            print(x, "--->", y)
-            pyautogui.click(x, y)
-            # yy = 100
-            # for i in range(100):
-            #     yy = yy+50
-            #     if yy > y:
-            #         return
-            #     pyautogui.click(200, yy)
-            time.sleep(20)
+# def xx():
+#     try:
+#         pyautogui.PAUSE = 1
+#         pyautogui.FAILSAFE = True
+#         width, height = pyautogui.size()
+#         while True:
+#             # x, y = pyautogui.position()
+#             # s = "X: " + str(x).rjust(4) + " Y: " + str(y) .rjust(4)
+#             # print(s, end="")
+#             # print("\b" * len(s), end="", flush=True)
+#             x = random.randint(1, width-1)
+#             y = random.randint(1, height-1)
+#             print(x, "--->", y)
+#             pyautogui.click(x, y)
+#             # yy = 100
+#             # for i in range(100):
+#             #     yy = yy+50
+#             #     if yy > y:
+#             #         return
+#             #     pyautogui.click(200, yy)
+#             time.sleep(20)
 
-    except pyautogui.FailSafeException:
+#     except pyautogui.FailSafeException:
 
-        print("Done")
-    except KeyboardInterrupt:
-        print("k")
+#         print("Done")
+#     except KeyboardInterrupt:
+#         print("k")
 
 
-if __name__ == "__main__":
-    xx()
-# pyautogui.PAUSE = 1
-# x, y = pyautogui.size()
-# yy = 100
-# for i in range(100):
-#     yy = yy+50
-#     if yy > y:
-#         break
-#     pyautogui.click(200, yy)
+# if __name__ == "__main__":
+#     xx()
+# # pyautogui.PAUSE = 1
+# # x, y = pyautogui.size()
+# # yy = 100
+# # for i in range(100):
+# #     yy = yy+50
+# #     if yy > y:
+# #         break
+# #     pyautogui.click(200, yy)
+import os.path
+s = os.path.abspath(__file__)
+a = os.path.dirname(s)
+print(a)
